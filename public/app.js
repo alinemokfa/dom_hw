fdd461427764e244370c1cd529e336db4e33378c
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-12-11 15:51:55 +0000

    cats can be added programmatically - NON-HACKY version

--- public/app.js
+++ public/app.js
@@ -10,7 +10,9 @@ const app = function () {
   listItemFavouriteFood.innerText = "Old fish";
 
   const catImage = document.createElement('li');
-  catImage.innerHTML = "<img width='500' src='http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg'/>";
+  const img = document.createElement('img');
+  catImage.appendChild(img);
+  img.src = "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg";
 
 
   catList.appendChild(listItemName);
@@ -25,5 +27,15 @@ const app = function () {
 
 
 };
+
+
+const addCat = function(author, text) {
+  const article = createArticle();
+  const blockquote = createBlockquote(text);
+  const cite = createCite(author);
+
+
+  appendElements(article, blockquote, cite);
+}
 
 document.addEventListener('DOMContentLoaded', app);
